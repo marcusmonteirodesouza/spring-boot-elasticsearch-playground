6a295367fa3b72af0dc817d4aae6911253052a2d
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-10-28 15:34:19 -0400

    Add elasticsearchplayground Spring Boot application

---
+++ elasticsearchplayground/src/main/java/com/marcusmonteirodesouza/elasticsearchplayground/ElasticSearchPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.marcusmonteirodesouza.elasticsearchplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchPlaygroundApplication.class, args);
+	}
+
+}
---
+++ elasticsearchplayground/src/test/java/com/marcusmonteirodesouza/elasticsearchplayground/ElasticSearchPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcusmonteirodesouza.elasticsearchplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
